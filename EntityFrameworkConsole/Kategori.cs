aaaa064769b0171869df6a5c5bbc63fdfbed7c9f
ozan <ekiciozann@gmail.com>
2023-10-20 12:23:00 +0300

    first commit

---
+++ EntityFrameworkConsole/Kategori.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkConsole
+{
+    public class Kategori
+    {
+        //varsayılan olarak otomatik olarak alır, değer göndermek gerekmez.PrimaryKey oldu Id dediğimiz için.
+        public int Id { get; set; }
+        public string KategoriAdi { get; set; }
+    }
+}
---
+++ EntityFrameworkConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Kategori k = new Kategori();
+            //k.Id = 1;
+            k.KategoriAdi = "Bilgisayar";
+
+            UrunContext db = new UrunContext();
+            db.Kategoriler.Add(k);
+            db.SaveChanges();
+            Console.WriteLine("DB'ye kayit edildi.");
+            Console.ReadLine();
+
+
+
+            //List<Kategori> kategoriler = new List<Kategori>()
+            //{
+            //    new Kategori(){Id=1 , KategoriAdi = "Telefon"},
+            //    new Kategori(){Id=2 , KategoriAdi = "Bilgisayar"},
+            //    new Kategori(){Id=3 , KategoriAdi = "Tablet"}
+            //};
+            //kategoriler.Add(k);
+        }
+    }
+}
